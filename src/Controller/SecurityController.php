c408ae72bbf622af83fac59be4dce4c90c5421e3
cassius269 <fahamygaston@gmail.com>
2024-10-15 15:01:14 +0200

    ajout de la fonctionnalité de connexion d'un utiilsateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -22,7 +23,7 @@ class SecurityController extends AbstractController
         name: 'inscription',
         methods: ['GET', 'POST']
     )]
-    public function subscribe(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         // Créer le formulaire
         $author = new Author;
@@ -40,7 +41,7 @@ class SecurityController extends AbstractController
             $hashedPassword = $passwordHasher->hashPassword($author, $plainTextPassword);
 
             // Ajouter les données manquantes à l'objet user des informations saisies par l'utilisateur
-            $author->setRoles(['ROLE_USER'])
+            $author->setRoles(['ROLE_AUTHOR'])
                 ->setCreatedAt(new \DateTimeImmutable())
                 ->setPassword($hashedPassword);
 
@@ -48,13 +49,13 @@ class SecurityController extends AbstractController
             $this->entityManager->persist($author);
             $this->entityManager->flush();
 
-            // Rediriger l'utilisateur à la page d'accueil 
+            // Rediriger l'utilisateur à la page d'accueil
             $this->addFlash('success', 'Votre compte a été créé'); // Emettre un message flash de succès d'inscription
             return $this->redirectToRoute('home'); // Rediriger à la page d'accueil de l'application
         }
         // Afficher la vue du formulaire
 
-        return $this->render('security/connexion.html.twig', [
+        return $this->render('security/inscription.html.twig', [
             'form' => $form->createView()
         ]);
     }
@@ -65,8 +66,31 @@ class SecurityController extends AbstractController
         name: 'connexion',
         methods: ['GET', 'POST']
     )]
-    public function connect(): Response
+    public function connect(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/connexion.html.twig', []);
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $username = $authenticationUtils->getLastUsername();
+
+        // Si un utilisateur est déjà connecté 
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home'); // Rediriger à la page d'accueil
+        }
+
+        // Si une erreur est survenue, émettre un message flash
+        if ($error) {
+            $this->addFlash('error', 'Une erreur s\'est produite');
+        }
+
+        return $this->render('security/connexion.html.twig', [
+            'error' => $error,
+            'username' => $username
+        ]);
     }
+
+
+    #[Route(
+        path: '/logout',
+        name: 'logout'
+    )]
+    public function logout() {}
 }
